5742facd1ee969a98b31f7413f703288172c7ed1
Omid Mojabi <omid.mojabi@gmail.com>
2023-03-10 18:31:22 +0100

    apply more tests on format checking

--- src/test/java/TestNumericUtilsMain.java
+++ src/test/java/TestNumericUtilsMain.java
@@ -20,6 +20,7 @@ public class TestNumericUtilsMain {
     @Test
     public void testUsualNumbersFormat(){
         assertEquals(true,NumbersUtils.isNumeric("100"));
+        assertEquals(true,NumbersUtils.isNumeric("999"));
         assertEquals(false,NumbersUtils.isNumeric("ABC"));
     }
 
