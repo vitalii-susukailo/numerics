3a5a69e9d91e4be3df4bf4498b9751149553a4a2
vitalii-susukailo <80201007+vitalii-susukailo@users.noreply.github.com>
2023-03-12 00:09:21 +0200

    Revert Changes

--- src/main/java/numerics/utils/NumbersUtils.java
+++ src/main/java/numerics/utils/NumbersUtils.java
@@ -82,8 +82,7 @@ public class NumbersUtils {
             }
         sumResult.setResult(result);
         sumResult.setLastDigit(nextDiv);
-        //return sumResult;
-        return 0;
+        return sumResult;
     }
 
     public static String getReversed(String inputStr) {
