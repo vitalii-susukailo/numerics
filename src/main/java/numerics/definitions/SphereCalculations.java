6105cfef9f14751cecaf13b07a9370ccb0f3e2c0
Omid Mojabi <omid.mojabi@gmail.com>
2023-03-28 21:06:03 +0300

    apply integration test

--- src/main/java/numerics/definitions/SphereCalculations.java
+++ src/main/java/numerics/definitions/SphereCalculations.java
@@ -14,12 +14,19 @@ public class SphereCalculations implements ShapesFactory {
         // area = 4 * PI * radius * radius
 
         // solution is dividing 2 steps: 4 * PI and radius * radius
-        return  null;
+        String firstPhrase = arbitraryNumbers.multiply("4",NumbersUtils.getPINumber());
+        String secondPhrase = arbitraryNumbers.multiply(dimensions.getRadius(),dimensions.getRadius());
+
+        return arbitraryNumbers.multiply(firstPhrase,secondPhrase);
     }
 
     @Override
     public String differenceOfAreas(ShapeDimensions dimensions1, ShapeDimensions dimensions2){
-        return null;
+
+        String firstShapeArea = this.surfaceArea(dimensions1);
+        String secondShapeArea = this.surfaceArea(dimensions2);
+
+        return arbitraryNumbers.subtraction(firstShapeArea,secondShapeArea);
     }
 
     @Override
