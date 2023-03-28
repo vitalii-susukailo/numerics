6810bf393203cceda66d24060c721bfca87d6f5d
Omid Mojabi <omid.mojabi@gmail.com>
2023-03-28 21:29:31 +0300

    success integration test

--- src/main/java/numerics/definitions/SphereCalculations.java
+++ src/main/java/numerics/definitions/SphereCalculations.java
@@ -12,18 +12,20 @@ public class SphereCalculations implements ShapesFactory {
 
     public String surfaceArea(ShapeDimensions dimensions) {
         // area = 4 * PI * radius * radius
-
         // solution is dividing 2 steps: 4 * PI and radius * radius
-        return null;
+        String first = arbitraryNumbers.multiply("4",NumbersUtils.getPINumber());
+        String second = arbitraryNumbers.multiply(dimensions.getRadius(),dimensions.getRadius());
+        return arbitraryNumbers.multiply(first,second);
     }
 
     @Override
     public String differenceOfAreas(ShapeDimensions dimensions1, ShapeDimensions dimensions2){
         // we want calculate diff of areas between the sun and the closest star! (proxima)
         // first calculate each of them area
-
+        String first = this.surfaceArea(dimensions1);//solar
+        String second = this.surfaceArea(dimensions2); //another
         //then calcualte diff
-        return null;
+        return arbitraryNumbers.subtraction(first,second);
     }
 
     @Override
